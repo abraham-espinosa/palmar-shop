fe698cf131fe1cffccbb3aab6a365cb752a22de7
abraham-1 <esp19005@byui.edu>
2023-07-06 03:40:43 -0600

    logo header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Navbar, Nav, Container} from 'react-bootstrap'
 import {FaShoppingCart, FaUser} from 'react-icons/fa'
+import logo from '../assets/logo.png'
 
 const Header = () => {
   return (
     <header>
         <Navbar bg='black' variant='dark' expand='sm' collapseOnSelect>
             <Container>
-                <Navbar.Brand href='/'>Palmar</Navbar.Brand>
+                <Navbar.Brand href='/'>
+                    <img src={logo} alt='logo'/>
+                    Palmar
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls='basic-navbar-nav' />
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
